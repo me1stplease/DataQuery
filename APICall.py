1b5f71f10a8efc76b0bf6842c90be3e03dc1349c
me1stplease <38397551+me1stplease@users.noreply.github.com>
2025-03-16 21:40:16 -0700

    Update APICall.py
    full change

--- APICall.py
+++ APICall.py
@@ -1,35 +1,29 @@
 from langchain_experimental.agents import create_pandas_dataframe_agent
 from langchain_google_genai.chat_models import ChatGoogleGenerativeAI
-from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
+import pandas as pd
+import os
+import google.generativeai as genai
 
 load_dotenv()
 
-llm = ChatGoogleGenerativeAI(model="gemini-pro", apiVersion: 'v1beta')
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
+llm = ChatGoogleGenerativeAI(model="gemini-pro")
 
-def GeminiCall(df,user, llmIn=llm):
+def GeminiCall(df, user, llmIn=llm):
     print()
     try:
         agent_executor = create_pandas_dataframe_agent(
-                llmIn,
-                df,
-                agent_type="zero-shot-react-description",
-                verbose=True,
-                return_intermediate_steps=True,
-                allow_dangerous_code=True
+            llmIn,
+            df,
+            agent_type="zero-shot-react-description",
+            verbose=True,
+            return_intermediate_steps=True,
+            allow_dangerous_code=True,
         )
-        # prompt = "You are a pandas query generator to generator or correct the pandas query based on the the following input "+"'"+user+"'"
-        # result = llmIn.invoke(prompt)
+        result = agent_executor.invoke({"input":user})
 
-        result = agent_executor.invoke(user)
-
-        # print("IN")
-
-        # print(result['output'])
-        # print(type(result.output))
         return result['output']
     except Exception as e:
-        # print(e.args[0])
-        return e.args[0]
-
+        return str(e)
